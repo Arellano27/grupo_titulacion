f2b3aed20f20a27dd3899c75151e41eac668b825
Gabriel Huayamabe <ghuayamabe89@gmail.com>
2015-10-19 19:58:24 -0500

    gh

--- appAcademico/src/Titulacion/SisAcademicoBundle/Helper/UgServices.php
+++ appAcademico/src/Titulacion/SisAcademicoBundle/Helper/UgServices.php
@@ -24,17 +24,17 @@ class UgServices
       $this->source     = "";
       /* PARAMETROS PARA SERVIDORES LOCALES EN UNIVERSIDAD - INICIO */
 
-     // $this->usuario       = "abc";
-     // $this->clave         = "123";
-     // $this->source        = "jdbc/procedimientosSaug";
-     // //$this->url           = "http://186.101.66.2:8080/";
-     // $this->url           = "http://192.168.100.11:8080/";
-     // $this->urlConsulta   = "consultas/ServicioWebConsultas?wsdl";
-     // $this->urlProcedim   = "WSObjetosUg/ServicioWebObjetos?wsdl";
-     // $this->urlWS         = "";
-     // //$this->host          = "186.101.66.2:8080";
-     // $this->host          = "192.168.100.11:8080";
-     // $this->sourceConsultas= "jdbc/consultasSaug";
+//      $this->usuario       = "abc";
+//      $this->clave         = "123";
+//      $this->source        = "jdbc/procedimientosSaug";
+//      //$this->url           = "http://186.101.66.2:8080/";
+//      $this->url           = "http://192.168.100.11:8080/";
+//      $this->urlConsulta   = "consultas/ServicioWebConsultas?wsdl";
+//      $this->urlProcedim   = "WSObjetosUg/ServicioWebObjetos?wsdl";
+//      $this->urlWS         = "";
+//      //$this->host          = "186.101.66.2:8080";
+//      $this->host          = "192.168.100.11:8080";
+//      $this->sourceConsultas= "jdbc/consultasSaug";
 
 
 
